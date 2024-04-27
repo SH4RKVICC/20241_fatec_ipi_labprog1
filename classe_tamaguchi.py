a197921f7cb89e4f8ee2f14190d5aabdd3302aec
SH4RKVICC <ttoiav.aamaral@gmail.com>
2024-04-26 23:18:52 -0300

    implementando classe tamaguchi

---
+++ classe_tamaguchi.py
@@ -0,0 +1,7 @@
+#criando classe tamaguchi com nome, fome, saude e idade
+class Tamaguchi:
+    def __init__ (self, nome, fome, saude, idade):
+        self.nome = nome
+        self.fome = fome
+        self.saude = saude
+        self.idade = idade
